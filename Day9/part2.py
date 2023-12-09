87f724315d8199fc530b67616a386e3c5080aae4
Jean F Gomez <jexan13@gmail.com>
2023-12-09 15:03:28 -0400

    Add Day 9 task

---
+++ Day9/part2.py
@@ -0,0 +1,38 @@
+from itertools import starmap, pairwise
+import operator
+
+numbers = []
+
+def decode_lines(line: str):
+    curent_numbers = list(map(int, line.split()))
+    numbers.append([curent_numbers])
+
+
+def get_diffs(listing):
+    return list(starmap(operator.sub, pairwise(listing)))
+        
+
+def extrapolate(num_list):
+    for i, nums in enumerate(num_list[-2::-1], 1):
+        nums.insert(0, nums[0] + num_list[-i][0])
+        
+    return num_list[0][0]
+        
+
+def decode_message():
+    with open("input.txt") as f:
+        for l in f.readlines():
+            decode_lines(l)
+     
+    for num_list in numbers:
+        current = num_list[0]
+        while any(current):
+            current = get_diffs(current)
+            num_list.append(current)
+        
+    total = sum(extrapolate(nums) for nums in numbers)
+    return total
+        
+            
+result = decode_message()
+print(result)
